a55da595cbd40581223bb7af43aab2630ac45c9f
programmerjake <programmerjake@gmail.com>
2014-08-15 22:43:14 -0700

    adding ffmpeg output

--- ffmpeg_renderer.cpp
+++ ffmpeg_renderer.cpp
@@ -1,6 +1,8 @@
 #include "ffmpeg_renderer.h"
 #include <memory>
 #include <libavcodec/avcodec.h>
+#include <libswscale/swscale.h>
+#include <fstream>
 
 using namespace std;
 
@@ -50,6 +52,8 @@ void setFFmpegOutputCodec(string codec)
 
 void setFFmpegFrameRate(float fps)
 {
+    if(fps < 1)
+        throw logic_error("ffmpeg frame rate too small");
     ffmpegFrameRate = fps;
 }
 
@@ -67,9 +71,40 @@ struct Writer
     Writer(const Writer &) = delete;
     void operator =(const Writer &) = delete;
     virtual void write(const void *buffer, size_t size) = 0;
+    virtual void flush() = 0;
     virtual void close() = 0;
     virtual ~Writer()
     {
+    }
+};
+
+class FileWriter : public Writer
+{
+private:
+    ofstream os;
+public:
+    FileWriter(string fileName)
+        : os(fileName.c_str(), ios::binary)
+    {
+        if(!os)
+            throw runtime_error("can't open " + fileName);
+    }
+
+    virtual void write(const void *buffer, size_t size) override
+    {
+        os.write((const char *)buffer, size);
+        if(!os)
+            throw runtime_error("can't write to file");
+    }
+
+    virtual void flush() override
+    {
+        os.flush();
+    }
+
+    virtual void close() override
+    {
+        os.close();
     }
 };
 
@@ -96,27 +131,109 @@ class FFmpegRenderer : public WindowRenderer
         }
         return nullptr;
     }
-    static AVCodec *findCodec()
+    void calcTimeBase()
     {
-        AVCodec *retval = findCodec("mpeg4");
-        if(retval)
-            return retval;
-        return findCodec("");
+        if(theCodec->supported_framerates == nullptr)
+        {
+            if(abs(frameRate - floor(frameRate)) < 1e-4)
+            {
+                theCodecContext->time_base = (AVRational){1, (int)floor(frameRate + 0.5)};
+            }
+            else if(abs(frameRate - 24.0 * 1000 / 1001) < 1e-4)
+            {
+                theCodecContext->time_base = (AVRational){1001, 24000};
+            }
+            else
+            {
+                theCodecContext->time_base = (AVRational){0, 0};
+                for(int denom = 1; denom <= 1001; denom++)
+                {
+                    if(abs(frameRate * denom - floor(frameRate * denom)) < 1e-4)
+                    {
+                        theCodecContext->time_base = (AVRational){denom, (int)floor(frameRate * denom + 0.5)};
+                        break;
+                    }
+                }
+                if(theCodecContext->time_base.num == 0)
+                {
+                    int num = 1 << 14;
+                    int denom = (int)floor(frameRate * (1 << 14) + 0.5);
+                    int v = num | denom; // or together so smallest set bit is divisor of both
+                    int powerOf2Factor = v - (v & (v - 1)); // find smallest set bit
+                    num /= powerOf2Factor;
+                    denom /= powerOf2Factor;
+                    theCodecContext->time_base = (AVRational){num, denom};
+                }
+            }
+        }
+        else
+        {
+            theCodecContext->time_base = (AVRational){0, 0};
+            float closestDistance = -1;
+            for(const AVRational *currentTimeBase = theCodec->supported_framerates; currentTimeBase->num != 0 && currentTimeBase->den != 0; currentTimeBase++)
+            {
+                float fpsValue = (float)currentTimeBase->den / currentTimeBase->num;
+                float distance = abs(fpsValue - frameRate);
+                if(closestDistance < 0 || distance < closestDistance)
+                {
+                    theCodecContext->time_base = *currentTimeBase;
+                    closestDistance = distance;
+                }
+            }
+            if(closestDistance == -1)
+                throw logic_error("ffmpeg codec has no valid time bases");
+        }
     }
     AVCodec *theCodec;
     AVCodecContext *theCodecContext;
+    AVFrame *picture;
+    SwsContext *swscaleContext;
+    SwsFilter *defaultFilter;
     shared_ptr<Writer> theWriter;
     shared_ptr<ImageRenderer> imageRenderer;
     size_t w, h;
-    float aspectRatio;
+    float aspectRatio, frameRate;
 public:
-    FFmpegRenderer(shared_ptr<Writer> theWriter)
-        : theWriter(theWriter)
+    FFmpegRenderer(shared_ptr<Writer> theWriter, string codec = getFFmpegOutputCodec(), string format = getFFmpegOutputFormat(), float frameRate = getFFmpegFrameRate())
+        : theWriter(theWriter), frameRate(frameRate)
     {
         w = getDefaultRendererWidth();
         h = getDefaultRendererHeight();
         aspectRatio = getDefaultRendererAspectRatio();
         imageRenderer = makeImageRenderer(w, h, aspectRatio);
+        theCodec = findCodec(codec);
+        if(theCodec == nullptr)
+            throw runtime_error("can't find codec " + codec);
+        theCodecContext = avcodec_alloc_context3(theCodec);
+        if(theCodecContext == nullptr)
+        {
+            throw runtime_error("can't create codec context");
+        }
+        theCodecContext->width = w;
+        theCodecContext->height = h;
+        calcTimeBase();
+        assert(theCodec->pix_fmts);
+        theCodecContext->pix_fmt = *theCodec->pix_fmts;
+        picture = avcodec_alloc_frame();
+        if(picture == nullptr)
+        {
+            avcodec_close(theCodecContext);
+            av_free(theCodecContext);
+            throw runtime_error("can't allocate frame");
+        }
+        SwsFilter *defaultFilter = sws_getDefaultFilter(0, 0, 0, 0, 0, 0, 0);
+        swscaleContext = sws_getCachedContext(nullptr, w, h, PIX_FMT_RGB32, w, h, theCodecContext->pix_fmt, 0, defaultFilter, defaultFilter, nullptr);
+    }
+    virtual ~FFmpegRenderer()
+    {
+        #error add new objects to destructor
+        avcodec_close(theCodecContext);
+        av_free(theCodecContext);
+        av_free(picture);
+    }
+    FFmpegRenderer(string fileName = getFFmpegOutputFile(), string codec = getFFmpegOutputCodec(), string format = getFFmpegOutputFormat(), float frameRate = getFFmpegFrameRate())
+        : FFmpegRenderer(make_shared<FileWriter>(fileName), codec, format, frameRate)
+    {
     }
     virtual ~LibAARenderer()
     {
